9b977cc81717b9d66515761c572f36b23abf9461
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-07-18 20:31:53 +0300

    Register the download registry action

---
+++ srv/update/cmd_download_registry.js
@@ -0,0 +1,13 @@
+
+module.exports = function make_download_registry() {
+  return async function download_registry(msg) {
+    return {
+      ok: false,
+      data: {
+        message: 'downloading not implemented'
+      }
+    }
+  }
+}
+
+
