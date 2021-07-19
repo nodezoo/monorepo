cb369fd71a4deeeeff942a0b404adce0949e4844
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-07-19 06:10:13 +0300

    Handle uncaught errors properly

--- srv/update/cmd_download_registry.js
+++ srv/update/cmd_download_registry.js
@@ -6,65 +6,76 @@ module.exports = function make_download_registry() {
   return async function download_registry(msg) {
     const seneca = this
 
-    if (seneca.root.context.is_downloading) {
-      return {
-        ok: false,
-        why: 'The download is already in progress.'
-      }
-    }
-
-    const response = await Axios.get('https://replicate.npmjs.com/_all_docs', {
-      responseType: 'stream'
-    })
-
-
-    seneca.root.context.is_downloading = true
-
-    response.data
-      .pipe(JsonStream.parse(['rows', true]))
-      .on('data', pkg_data => {
-        if (!seneca.root.context.is_downloading) {
-          /* NOTE: If a download has been aborted, this code will be called
-           * multiple times per response. That's okay, the #destroy() method
-           * becomes a no-op after the first call - it's not like double
-           * "free"-ing of a pointer in C.
-           */
-
-          response.data.destroy()
-
-          return
+    try {
+      if (seneca.root.context.is_downloading) {
+        return {
+          ok: false,
+          why: 'The download is already in progress.'
         }
+      }
 
-        const { id: pkg_id } = pkg_data
+      const response = await Axios.get('https://replicate.npmjs.com/_all_docs', {
+        responseType: 'stream'
+      })
 
-        return seneca.make('nodezoo', 'npm')
-          .data$({ name: pkg_id })
-          .save$(err => {
-            if (err) {
-              seneca.log.error(err.message)
-              return
-            }
+
+      seneca.root.context.is_downloading = true
+
+      response.data
+        .pipe(JsonStream.parse(['rows', true]))
+        .on('data', pkg_data => {
+          if (!seneca.root.context.is_downloading) {
+            /* NOTE: If a download has been aborted, this code will be called
+             * multiple times per response. That's okay, the #destroy() method
+             * becomes a no-op after the first call - it's not like double
+             * "free"-ing of a pointer in C.
+             */
+
+            response.data.destroy()
 
             return
-          })
-      })
-      .once('close', () => {
-        seneca.root.context.is_downloading = false
-      })
-      .once('error', err => {
-        /* NOTE: Since the 'close' event is always triggered after the 'error'
-         * event (or at least that's the way it's supposed to be) - we leave it
-         * up to the handler of the 'close' event to set the `is_downloading` flag
-         * to false.
-         */
+          }
 
-        seneca.log.error(err.message)
-      })
+          const { id: pkg_id } = pkg_data
+
+          return seneca.make('nodezoo', 'npm')
+            .data$({ name: pkg_id })
+            .save$(err => {
+              if (err) {
+                seneca.log.error(err.message)
+                return
+              }
+
+              return
+            })
+        })
+        .once('close', () => {
+          seneca.root.context.is_downloading = false
+        })
+        .once('error', err => {
+          /* NOTE: Since the 'close' event is always triggered after the 'error'
+           * event (or at least that's the way it's supposed to be) - we leave it
+           * up to the handler of the 'close' event to set the `is_downloading` flag
+           * to false.
+           */
+
+          seneca.log.error(err.message)
+        })
 
 
-    return {
-      ok: true,
-      data: { message: 'Downloading...' }
+      return {
+        ok: true,
+        data: { message: 'Downloading...' }
+      }
+    } catch (err) {
+      seneca.log.error(err.message)
+
+      seneca.root.context.is_downloading = false
+
+      return {
+        ok: false,
+        why: 'Something went wrong. Please check the logs for more information.'
+      }
     }
   }
 }
