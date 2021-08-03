65db087c6d95c23978355215b557821af745532f
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-08-03 11:21:24 +0300

    Fix the link size

--- srv/web/www/src/components/PkgSummaryCard.vue
+++ srv/web/www/src/components/PkgSummaryCard.vue
@@ -4,9 +4,11 @@
     tile
     color="grey lighten-5"
   >
-    <router-link :to="'/pkgs/' + pkg_name">
-      <v-card-title>{{ pkg_name }}</v-card-title>
-    </router-link>
+    <v-card-title>
+      <router-link :to="'/pkgs/' + pkg_name">
+        {{ pkg_name }}
+      </router-link>
+    </v-card-title>
     <v-card-subtitle>0.0.0_VER_PLAC</v-card-subtitle>
     <v-card-text>lorem_DESC_PLAC</v-card-text>
   </v-card>
