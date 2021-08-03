6304c56d2cc9afb2680ceb5b3a26aec8e24ccaa4
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-08-03 10:18:35 +0300

    Extract the nav header into a separate component for future reuse

--- srv/web/www/src/views/Home.vue
+++ srv/web/www/src/views/Home.vue
@@ -1,26 +1,10 @@
 <template>
   <v-container>
 
-    <v-row v-if="$session && $session.exists()">
-      <v-col>
-        <router-link to="/logout">Sign out</router-link>
-        <router-link to="/me/favorites">Favorites</router-link>
-      </v-col>
-    </v-row>
-
-    <v-row v-else>
-      <v-col>
-        <router-link to="/login">Sign in</router-link>
-      </v-col>
-    </v-row>
-
     <v-row>
-      <v-col class="text-center">
-        <v-form @submit.prevent="onSubmitSearch">
-          <v-text-field label="Search" v-model="search" @input="onSearchInput" />
-        </v-form>
-      </v-col>
+      <NavHeader @searching="searchForPkgs"/>
     </v-row>
+
     <v-row v-for="pkg in pkgs" :key="pkg.name">
       <v-col>
         <PkgSummaryCard :pkg_name="pkg.name" />
@@ -32,9 +16,7 @@
 <script>
   import Api from '@/lib/api'
   import PkgSummaryCard from '@/components/PkgSummaryCard.vue'
-
-
-  const TYPING_CHECK_WAIT_MS = 300
+  import NavHeader from '@/components/NavHeader.vue'
 
 
   export default {
@@ -42,53 +24,23 @@
 
 
     data: () => ({
-      pkgs: [],
-      search: '',
-      last_typed_at: null
+      pkgs: []
     }),
 
 
     methods: {
-      async searchForPkgs() {
+      async searchForPkgs(args) {
+        const { search } = args
+
         this.pkgs = await Api
-          .listPkgsWithNamePrefix({ prefix: this.search })
+          .listPkgsWithNamePrefix({ prefix: search })
           .then(res => res.data.pkgs)
       },
-
-
-      async onSubmitSearch() {
-        await this.searchForPkgs()
-      },
-
-
-      isCurrentlyTyping() {
-        if (!this.last_typed_at) {
-          return false
-        }
-
-        const now = new Date()
-        const last_typed = now.getTime() - this.last_typed_at.getTime()
-
-        return last_typed < TYPING_CHECK_WAIT_MS
-      },
-
-
-      async onSearchInput() {
-        const self = this
-        self.last_typed_at = new Date()
-
-        setTimeout(async () => {
-          if (self.isCurrentlyTyping()) {
-            return
-          }
-
-          await self.searchForPkgs()
-        }, 300)
-      }
     },
 
 
     components: {
+      NavHeader,
       PkgSummaryCard
     }
   }
