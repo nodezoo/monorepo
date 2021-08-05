49255bde9617b7aa446ace2c0214e4a00ec2a55b
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-08-05 23:49:04 +0300

    Sketch up the package charts

--- srv/web/www/src/views/ShowPkg.vue
+++ srv/web/www/src/views/ShowPkg.vue
@@ -1,14 +1,45 @@
 <template>
   <div>
     <h1>{{ pkg_name }}</h1>
-    <PkgHistoryChart />
+    <div>
+      <LineChart
+
+        :chartData="{
+          labels: ['30 Jul', '31 Jul', '1 Aug', '2 Aug', '3 Aug', '4 Aug', '5 Aug'],
+          datasets: [{
+            label: 'Downloads by day',
+            data: [60, 50, 45, 47, 37, 51, 55],
+            fill: false,
+            borderColor: 'rgb(0, 0, 255)',
+            tension: 0.1
+          }]
+        }"
+
+        :options="{ maintainAspectRatio: false }" />
+
+
+      <LineChart
+
+        :chartData="{
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+          datasets: [{
+            label: 'Downloads by month',
+            data: [200, 350, 320, 400, 120, 425, 370, 410],
+            fill: false,
+            borderColor: 'rgb(0, 0, 255)',
+            tension: 0.1
+          }]
+        }"
+
+        :options="{ maintainAspectRatio: false }" />
+    </div>
   </div>
 </template>
 
 
 <script>
   import Api from '@/lib/api'
-  import PkgHistoryChart from '@/components/PkgHistoryChart.vue'
+  import LineChart from '@/components/LineChart.vue'
 
 
   export default {
@@ -20,7 +51,7 @@
 
     mounted() {
       // TODO: Implementation.
-      // TODO: Redirect to the 404 page when the package does not exist.
+      // TODO: Redirect to the 404 page if the package does not exist.
       //
 
       const { params: { name: pkg_name } } = this.$route
@@ -29,7 +60,7 @@
     },
 
     components: {
-      PkgHistoryChart
+      LineChart
     }
   }
 </script>
