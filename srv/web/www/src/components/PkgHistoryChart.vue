9e83375bddd9d5682775d6ad487293fb76deb32f
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-08-06 04:59:59 +0300

    Extract the history chart into a separate component

---
+++ srv/web/www/src/components/PkgHistoryChart.vue
@@ -0,0 +1,42 @@
+<template>
+  <LineChart
+
+    :chartData="{
+      labels: ['30 Jul', '31 Jul', '1 Aug', '2 Aug', '3 Aug', '4 Aug', '5 Aug'],
+      datasets: [
+        {
+          label: 'Downloads by day',
+          data: [60, 50, 45, 47, 37, 51, 55],
+          fill: false
+        },
+        {
+          label: 'Forks by day'
+        }
+      ]
+    }"
+
+    :options="{ maintainAspectRatio: false }" />
+</template>
+
+
+<script>
+  import Api from '@/lib/api'
+  import LineChart from '@/components/LineChart.vue'
+
+
+  export default {
+    name: 'PkgHistoryChart',
+
+    data: () => ({
+      pkg_name: ''
+    }),
+
+    mounted() {
+    },
+
+    components: {
+      LineChart
+    }
+  }
+</script>
+
--- srv/web/www/src/views/ShowPkg.vue
+++ srv/web/www/src/views/ShowPkg.vue
@@ -2,36 +2,7 @@
   <div>
     <h1>{{ pkg_name }}</h1>
     <div>
-      <LineChart
-
-        :chartData="{
-          labels: ['30 Jul', '31 Jul', '1 Aug', '2 Aug', '3 Aug', '4 Aug', '5 Aug'],
-          datasets: [{
-            label: 'Downloads by day',
-            data: [60, 50, 45, 47, 37, 51, 55],
-            fill: false,
-            borderColor: 'rgb(0, 0, 255)',
-            tension: 0.1
-          }]
-        }"
-
-        :options="{ maintainAspectRatio: false }" />
-
-
-      <LineChart
-
-        :chartData="{
-          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-          datasets: [{
-            label: 'Downloads by month',
-            data: [200, 350, 320, 400, 120, 425, 370, 410],
-            fill: false,
-            borderColor: 'rgb(0, 0, 255)',
-            tension: 0.1
-          }]
-        }"
-
-        :options="{ maintainAspectRatio: false }" />
+      <PkgHistoryChart :pkg_name="pkg_name" />
     </div>
   </div>
 </template>
@@ -39,7 +10,7 @@
 
 <script>
   import Api from '@/lib/api'
-  import LineChart from '@/components/LineChart.vue'
+  import PkgHistoryChart from '@/components/PkgHistoryChart.vue'
 
 
   export default {
@@ -60,7 +31,7 @@
     },
 
     components: {
-      LineChart
+      PkgHistoryChart
     }
   }
 </script>
