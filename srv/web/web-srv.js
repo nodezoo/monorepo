bd6e67c63bbd585aad6b449eae7375a83e2f1771
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-08-09 10:34:48 +0300

    Run client separately from backend

--- srv/web/web-srv.js
+++ srv/web/web-srv.js
@@ -5,33 +5,7 @@ const MakeServer = require('./server/server')
 
 function web(options) {
   const seneca = this
-
-
-
-  const vue_app = Express()
-
-
-  const ASSETS_PATH = Path.join(__dirname, 'www', 'src', 'assets')
-  vue_app.use('/assets', Express.static(ASSETS_PATH))
-
-
-  const VIEWS_PATH = Path.join(__dirname, 'www', 'dist')
-  vue_app.use(Express.static(VIEWS_PATH))
-
-
-  vue_app.get('/*', (req, res) => {
-    const index = Path.join(VIEWS_PATH, 'index.html')
-    return res.sendFile(index)
-  })
-
-
-  vue_app.listen(8080)
-
-
-
-
-  const app = MakeServer({ seneca: this })
-
+  const app = MakeServer({ seneca }) 
 
   app.listen(9000)
 }
