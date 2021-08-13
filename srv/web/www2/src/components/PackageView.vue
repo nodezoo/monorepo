9157c653fc60f1f0ba58e12165d64d3fcccaa305
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-08-13 14:15:10 +0300

    Implement the favorite packages page

--- srv/web/www2/src/components/PackageView.vue
+++ srv/web/www2/src/components/PackageView.vue
@@ -1,12 +1,57 @@
 <template>
   <div>
-    <h1>Package View</h1>
+    <v-container>
+      <v-row v-for="pkg in pkgs" :key="pkg.name">
+        <v-col>
+          <PackageSummaryCard :pkg_name="pkg.name" /> </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
+import Api from '@/lib/api'
+import PackageSummaryCard from '@/components/PackageSummaryCard.vue'
+
+
 export default {
-  name: 'PackageView'
+  name: 'PackageView',
+
+
+  data: () => ({
+    pkgs: []
+  }),
+
+
+  async mounted() {
+    await this.listMyBookmarks()
+  },
+
+
+  methods: {
+    async listMyBookmarks() {
+      /*
+      const auth_token = this.$store.state.auth?.token
+      */
+
+
+      const bookmarksResponse = await Api.listMyBookmarkedPkgs({
+        auth_token: 'd2dd8fc5-b962-4a3d-a899-639962cbbb00'
+      })
+
+      if (!bookmarksResponse.data.ok) {
+        return
+      }
+
+      const { pkgs } = bookmarksResponse.data
+      this.pkgs = pkgs
+    }
+  },
+
+
+  components: {
+    PackageSummaryCard
+  }
 }
 </script>
 
--- srv/web/www2/src/index.js
+++ srv/web/www2/src/index.js
@@ -1,12 +1,8 @@
 import Vue from 'vue'
-import VueSession from 'vue-session'
 import Public from './Public.vue'
 
 Vue.config.productionTip = false
 Vue.config.devtools = true
-
-
-Vue.use(VueSession)
 
 
 new Vue({
