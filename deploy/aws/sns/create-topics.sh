7d36ec250ad9091a9f09d0ce2728d1eca94af61d
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-08-16 06:17:59 +0300

    Remove old API

--- deploy/aws/sns/create-topics.sh
+++ deploy/aws/sns/create-topics.sh
@@ -9,7 +9,6 @@ aws sns create-topic --name nodezoo_bookmark_pkg_role_web_scope_account
 aws sns create-topic --name nodezoo_list_bookmarks_role_web_scope_account
 aws sns create-topic --name nodezoo_is_premium_role_web_scope_account
 aws sns create-topic --name nodezoo_checkout_for_premium_role_web_scope_account_submit_checkout
-aws sns create-topic --name nodezoo_join_premium_role_web_scope_account
 aws sns create-topic --name nodezoo_collect_part_role_info
 aws sns create-topic --name nodezoo_get_info_role_info
 aws sns create-topic --name nodezoo_role_info_test_true
