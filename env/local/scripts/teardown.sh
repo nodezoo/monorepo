c3502cf397c55f4f6858b48aae80443320ab92da
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-08-29 01:03:58 +0300

    Make it finally work with the mock APIs

--- env/local/scripts/teardown.sh
+++ env/local/scripts/teardown.sh
@@ -7,6 +7,18 @@ set -euo pipefail
 IFS=$'\n\t'
 
 
+# There is currently no need to tear down the frontend in the local env.
+#
 
 
+# Tearing down the mock npm API
+#
+docker-compose -f ./env/local/devassets/mock-api-npm/docker-compose.yaml down
+rm ./env/local/devassets/mock-api-npm
+
+
+# Tearing down the mock GitHub API
+#
+docker-compose -f ./env/local/devassets/mock-api-github/docker-compose.yaml down
+rm ./env/local/devassets/mock-api-github
 
