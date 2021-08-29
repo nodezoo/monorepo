d8e96a920d3d14c6e13374b70c754bc4e8a0b6e9
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-08-29 19:28:57 +0300

    Fix a typo in the script

--- env/local/scripts/teardown.sh
+++ env/local/scripts/teardown.sh
@@ -14,11 +14,11 @@ IFS=$'\n\t'
 # Tearing down the mock npm API
 #
 docker-compose -f ./env/local/devassets/mock-api-npm/docker-compose.yaml down
-rm ./env/local/devassets/mock-api-npm
+rm ./env/local/devassets/mock-api-npm -rf
 
 
 # Tearing down the mock GitHub API
 #
 docker-compose -f ./env/local/devassets/mock-api-github/docker-compose.yaml down
-rm ./env/local/devassets/mock-api-github
+rm ./env/local/devassets/mock-api-github -rf
 
