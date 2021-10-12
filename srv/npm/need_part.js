8222c4fc6c029c2686bcaaa22bef3d62036802e5
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-10-13 01:02:35 +0300

    Restore the addition of documents to the search pool

--- srv/npm/need_part.js
+++ srv/npm/need_part.js
@@ -1,3 +1,4 @@
+const Assert = require('assert')
 
 
 module.exports = function make_need_part() {
@@ -12,6 +13,7 @@ module.exports = function make_need_part() {
 
     if (!res.ok) {
       console.error(res)
+      Assert.fail('get:package')
       return
     }
 
@@ -20,6 +22,40 @@ module.exports = function make_need_part() {
       part: 'npm',
       data: res.pkg
     })
+
+
+    // NOTE: Because the package may already be in the search pool,
+    // we should then overwrite it in the search pool.
+    //
+    // sys:search,cmd:remove is required to not crash by its
+    // contract, so it should be perfectly fine to try to
+    // remove a document from the search pool that does not
+    // exist.
+
+    Assert(null != res.pkg, 'res.pkg')
+
+    const doc = {
+      ...res.pkg,
+      id: pkg_name
+    }
+
+    const added = await seneca.post('sys:search,cmd:add', { doc })
+      .catch(async (err) => {
+        console.error(err.message)
+
+        await seneca.post('sys:search,cmd:remove', {
+          id: pkg_name
+        })
+
+        return seneca.post('sys:search,cmd:add', { doc })
+      })
+
+    if (!added.ok) {
+      console.error(added)
+      Assert.fail('sys:search,cmd:add')
+      return
+    }
+
 
     return
   }
