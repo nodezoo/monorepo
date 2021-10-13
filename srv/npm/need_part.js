f4275e5547fb0957c1bf1035183477a024b4cdc3
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-10-13 20:21:49 +0300

    Refactor the search logic

--- srv/npm/need_part.js
+++ srv/npm/need_part.js
@@ -24,48 +24,6 @@ module.exports = function make_need_part() {
       data: res.pkg
     })
 
-
-    // NOTE: Because the package may already be in the search pool,
-    // we should then overwrite it in the search pool.
-    //
-    // sys:search,cmd:remove is required by its contract to not crash,
-    // hence it should be perfectly fine to try to remove a document
-    // from the search pool that does not exist.
-
-    Assert(null != res.pkg, 'res.pkg')
-
-    const doc = {
-      id: pkg_name,
-
-      name: pkg_name,
-
-      ...pick(res.pkg, [
-        'name',
-        'version',
-        'giturl',
-        'desc',
-        'readme'
-      ])
-    }
-
-    const added = await seneca.post('sys:search,cmd:add', { doc })
-      .catch(async (err) => {
-        console.error(err.message)
-
-        await seneca.post('sys:search,cmd:remove', {
-          id: pkg_name
-        })
-
-        return seneca.post('sys:search,cmd:add', { doc })
-      })
-
-    if (!added.ok) {
-      console.error(added)
-      Assert.fail('sys:search,cmd:add')
-      return
-    }
-
-
     return
   }
 }
